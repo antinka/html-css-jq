87a676e5dc355d014b170db791c937bbbcc16af9
antinka <antinka.katrinka@gmail.com>
2015-12-12 13:10:06 +0200

    add file

--- MyCalculator.js
+++ MyCalculator.js
@@ -1,26 +1,32 @@
-function EnterNumber(num ){
-	if($("input[name=res]").val() == "0" || $("input[name=res]").val() == fistNum )
-	{
-		$("input[name=res]").val() = num;
-	}	
-	else
-		$("input[name=res]").val() += num;	
-}
-function Clear () {
-	$("input[name=res]").val() = "0";
-}
-function Decimal(){
-
-	var temp = ".";
-	$("input[name=res]").val().add("temp");
-}
-function PlusMinus(){
-	$("input[name=res]").val() *=-1;
-}
 var fistNum = 0;
 var secondNum = 0;
 var opperation;
 var result = 0;
+var temp;
+function EnterNumber(num ){
+ 
+	if($("input[name=res]").val() == "" || $("input[name=res]").val() == fistNum){
+		$("input[name=res]").val(num) ;
+	}	
+	else {
+		temp = $("input[name=res]").val();
+		temp += num;
+		$("input[name=res]").val(temp) ;
+	}
+}
+function Clear () {
+	$("input[name=res]").val("");
+}
+function PlusMinus(){
+	temp = $("input[name=res]").val();
+	temp *=-1;
+	$("input[name=res]").val(temp);
+}
+function Decimal(){
+	temp = $("input[name=res]").val();
+	temp += ".";
+	$("input[name=res]").val(temp);
+}
 
 function CallOperration(oper)
 {
@@ -34,12 +40,14 @@ function CallOperration(oper)
 		if(opperation == '+')
 		{
 			secondNum = $("input[name=res]").val();
-			$("input[name=res]").val() = +fistNum + +secondNum ;
+			temp = +fistNum + +secondNum 
+			$("input[name=res]").val(temp) ;
 		}
 		else if(opperation == '*')
 		{
 			secondNum = $("input[name=res]").val();
-			$("input[name=res]").val() = +fistNum * +secondNum ;
+			temp = +fistNum * +secondNum;
+			$("input[name=res]").val(temp)  ;
 		}
 		else if(opperation == '/')
 		{
@@ -47,12 +55,17 @@ function CallOperration(oper)
 			if(secondNum == "0")
 				{alert("делить на 0 нельзя")}
 			else
-			$("input[name=res]").val() = +fistNum / +secondNum ;
+			{
+				temp = +fistNum / +secondNum;
+				$("input[name=res]").val(temp) ;
+			
+			}
 		}
 		else if(opperation == '-')
 		{
 			secondNum = $("input[name=res]").val();
-			$("input[name=res]").val() = +fistNum - +secondNum ;
+			temp = +fistNum - +secondNum;
+			$("input[name=res]").val(temp)  ;
 		}
 	}
 }
